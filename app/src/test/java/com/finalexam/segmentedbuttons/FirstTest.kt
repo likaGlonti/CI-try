9fdcfaf3d05aecb9ed3a86678faee4487a115ae8
likaGlonti <60490214+likaGlonti@users.noreply.github.com>
2022-12-22 15:21:05 +0400

    added test

---
+++ app/src/test/java/com/finalexam/segmentedbuttons/FirstTest.kt
@@ -0,0 +1,12 @@
+package com.finalexam.segmentedbuttons
+
+import org.junit.Assert
+import org.junit.Test
+
+class FirstTest {
+
+    @Test
+    fun `run run`() {
+        Assert.assertEquals(1, 1)
+    }
+}
